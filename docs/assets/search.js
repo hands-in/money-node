b3544c76b9c60a184f9908e4a794b9651e2ed820
FeLiNa22 <raulpatel97@gmail.com>
2023-07-04 10:33:32 +0000

    Deploying to gh-pages from @ Hands-In/money-node@8980b5462fd82ed82e664623314a5e9fe07992be 🚀

---
+++ docs/assets/search.js
@@ -0,0 +1,1 @@
+window.searchData = JSON.parse("{\"rows\":[{\"kind\":64,\"name\":\"getGreeting\",\"url\":\"functions/getGreeting.html\",\"classes\":\"\"}],\"index\":{\"version\":\"2.3.9\",\"fields\":[\"name\",\"comment\"],\"fieldVectors\":[[\"name/0\",[0,2.877]],[\"comment/0\",[]]],\"invertedIndex\":[[\"getgreeting\",{\"_index\":0,\"name\":{\"0\":{}},\"comment\":{}}]],\"pipeline\":[]}}");
