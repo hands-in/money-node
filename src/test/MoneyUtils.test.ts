95f38404b1d889817df974d4d53d15bdd89cd4af
Raul <raulpatel97@gmail.com>
2023-07-05 12:37:19 +0100

    test(moneyutils.test.ts): locales set explicitly in tests
    locales change on different machines so it must be set in tests to ensure the format is consistent

--- src/test/MoneyUtils.test.ts
+++ src/test/MoneyUtils.test.ts
@@ -1,24 +1,33 @@
 import {Currency} from '~/@types/Currency';
 import {MoneyUtils} from '~/index';
 
-describe('Money Utils', () => {
+describe('Money Utils (locale = en-GB)', () => {
+  const LOCALE = 'en-GB';
+
   it('expect formatMoney to return £300.00 when input is 300_00GBP', () => {
     const THREE_POUNDS = {amount: 300_00, currency: Currency.GBP};
 
-    expect(MoneyUtils.formatMoney(THREE_POUNDS)).toEqual('£300.00');
+    expect(MoneyUtils.formatMoney(THREE_POUNDS, {locales: LOCALE})).toEqual(
+      '£300.00'
+    );
   });
 
   it('expect formatMoney to return ¥30,000 when input is 30000JPY', () => {
     const THIRTY_THOUSAND_YEN = {amount: 30_000, currency: Currency.JPY};
 
-    expect(MoneyUtils.formatMoney(THIRTY_THOUSAND_YEN)).toEqual('¥30,000');
+    expect(
+      MoneyUtils.formatMoney(THIRTY_THOUSAND_YEN, {locales: LOCALE})
+    ).toEqual('¥30,000');
   });
 
   it('expect formatMoney to return "JP¥30,000" when input is 30000JPY and includeCurrencyCode is true', () => {
     const THIRTY_THOUSAND_YEN = {amount: 30_000, currency: Currency.JPY};
 
     expect(
-      MoneyUtils.formatMoney(THIRTY_THOUSAND_YEN, {includeCurrencyCode: true})
+      MoneyUtils.formatMoney(THIRTY_THOUSAND_YEN, {
+        locales: LOCALE,
+        includeCurrencyCode: true,
+      })
     ).toEqual('JP¥30,000');
   });
 });
