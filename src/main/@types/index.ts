c1af2dfa16d410f72e27daf26c4bd756f26b7664
Raul <raulpatel97@gmail.com>
2023-07-05 15:10:51 +0100

    fix(exported types for re-ues): exported @types

---
+++ src/main/@types/index.ts
@@ -0,0 +1,7 @@
+import {Currency} from './Currency';
+import Comparator from './Comparator';
+import {ExchangeRates} from './ExchangeRates';
+import {Money} from './Money';
+import MoneyCalculator from './MoneyCalculator';
+
+export {Currency, Comparator, Money, ExchangeRates, MoneyCalculator};
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,14 @@
+/* eslint-disable import/no-unused-modules */
 import MoneyUtils from './MoneyUtils';
 
-/* eslint-disable import/no-unused-modules */
 export {GlobalMoney, GlobalMoneyCalculator} from './global';
 export {LocalMoney, LocalMoneyCalculator} from './local';
 export {CurrencyUtils} from './currency';
+export {
+  Comparator,
+  Currency,
+  ExchangeRates,
+  Money,
+  MoneyCalculator,
+} from './@types';
 export {MoneyUtils};
